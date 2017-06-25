178d79b14d009841ca9cb6f35578cd79254d9568
Markus Mahr <carpenoctem@iMac-LAN.fritz.box>
2017-06-25 11:31:27 +0200

    Update to NEEO-SDK Code

--- index.js
+++ index.js
@@ -22,23 +22,33 @@ const customLightDevice = neeoapi.buildDevice('Simple Buttons')
   .addButton({ name: 'power-off', label: 'HS-PLUG OFF' })
   .addButtonHander(controller.onButtonPressed);
 
-console.log('- discover one NEEO Brain...');
-neeoapi.discoverOneBrain()
-  .then((brain) => {
-    console.log('- Brain discovered:', brain.name);
-
-    console.log('- Start server');
-    return neeoapi.startServer({
-      brain,
-      port: 6336,
-      name: 'simple-adapter-one',
-      devices: [customLightDevice]
-    });
+function startSdkExample(brain) {
+  console.log('- Start server');
+  neeoapi.startServer({
+    brain,
+    port: 6336,
+    name: 'simple-adapter-one',
+    devices: [customLightDevice]
   })
   .then(() => {
-    console.log('# READY! use the NEEO app to search for "TP-LINK HS100".');
+    console.log('# READY! use the NEEO app to search for "NEEO Simple Device".');
   })
-  .catch((err) => {
-    console.error('ERROR!', err);
+  .catch((error) => {
+    //if there was any error, print message out to console
+    console.error('ERROR!', error.message);
     process.exit(1);
-});
+  });
+}
+
+const brainIp = process.env.BRAINIP;
+if (brainIp) {
+  console.log('- use NEEO Brain IP from env variable', brainIp);
+  startSdkExample(brainIp);
+} else {
+  console.log('- discover one NEEO Brain...');
+  neeoapi.discoverOneBrain()
+    .then((brain) => {
+      console.log('- Brain discovered:', brain.name);
+      startSdkExample(brain);
+    });
+}
